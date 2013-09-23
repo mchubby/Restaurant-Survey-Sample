acb3a0c464688dd6b94b625b94703f1fd15cd723
TJ VanToll <TJ.VanToll@telerik.com>
2013-09-23 09:20:08 -0500

    Store form results in Everlive.

--- Restaurant Survey Sample/scripts/app/main.js
+++ Restaurant Survey Sample/scripts/app/main.js
@@ -17,19 +17,33 @@
         showAlert(message, 'Error occured');
         return true;
     });
-    
-    var applicationSettings = {
-        emptyGuid: '00000000-0000-0000-0000-000000000000',
-        apiKey: 'eVKxNui85A6TopjR'
-    };
+
 
     // initialize Everlive SDK
     var el = new Everlive({
-        apiKey: applicationSettings.apiKey
+        apiKey: 'eVKxNui85A6TopjR'
     });
     
     $('form').on('submit', function(event) {
         event.preventDefault();
-        console.log( $(this.location).val(), $(this.appetizer).val(), $(this.rating).val() );
+        var data = {
+            location: $(this.location).val(),
+            appetizer: $(this.appetizer).val() == "yes",
+            rating: $(this.rating).val()
+        };
+        
+        $.ajax({
+            type: "POST",
+            url: 'https://api.everlive.com/v1/eVKxNui85A6TopjR/Ratings',
+            headers: { "Authorization" : "Bearer ${AccessToken}" },
+            contentType: "application/json",
+            data: JSON.stringify(data),
+            success: function(data){
+                alert(JSON.stringify(data));
+            },
+            error: function(error){
+                alert('Sorry, an error occurred processing your survey. Please try again later.');
+            }
+        })
     });
 }());
